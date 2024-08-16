ee2dfc153a7adb28416cb908d57c8dcecda95df9
Isabel Cervantes <jarquincmayra@gmail.com>
2024-08-16 15:00:10 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Ejercicio para mi portafolio de habilidades de TECNOLOCHICAS PRO
+
+El presente proyecto es un portafolio desarrollado para poner en práctica las habilidades obtenidad dentro del bootcamp de desarrollo frontend de Technolochicas PRO.
+
+Fue desarrollado con HTML, CSS y JS con el uso de el framework de UI, Bootstrap utilizando además bibliotecas externas.
+
+La página es responsiva (adaptable a diferentes tamaños de pantalla) e incluye la presentación de la autora del proyecto.
+
+[Proyecto desplegado (https://portafolio-b7-g2-otpr.vercel.app/)](https://portafolio-b7-g2-otpr.vercel.app/)
+
+
+## Secciones de mi sitio
+
+![Presentación](assets/intro1.png)
+![Contenido](assets/contenido.png)
+![Contenido](assets/contenido2.png)
+![Contenido](assets/contenido3.png)
+![Contenido](assets/contenido4.png)
+![Contacto](assets/footer.png)
+
+## Tecnologías
+* HTML
+* CSS
+* Bootstrap
+* JS
+
+
+---
+Desarrollado con amor por Isabel, Tecnolochicas pro :)
--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const carousel = document.querySelector('.carousel');
-    const images = document.querySelectorAll('.figure-rollover');
-    const prevButton = document.querySelector('.prev');
-    const nextButton = document.querySelector('.next');
-    const numImagesPerSlide = 4; // Mostrar 4 imágenes por vez
-    const totalImages = images.length;
-    let currentIndex = 0;
-
-    function updateCarousel() {
-        carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
-
-    prevButton.addEventListener('click', () => {
-        currentIndex = (currentIndex - 1 + Math.ceil(totalImages / numImagesPerSlide)) % Math.ceil(totalImages / numImagesPerSlide);
-        updateCarousel();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
     });
+  });
 
-    nextButton.addEventListener('click', () => {
-        currentIndex = (currentIndex + 1) % Math.ceil(totalImages / numImagesPerSlide);
-        updateCarousel();
-    });
-});
+  
+
+  
